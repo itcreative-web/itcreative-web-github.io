c593b40d49dafab89915b7d8b60fa101d78471a1
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-03-25 16:50:06 +0200

    fix

--- kostya/puma/js/main.js
+++ kostya/puma/js/main.js
@@ -112,7 +112,7 @@ $(function () {
 	}
 
 	carousel(".galary__wrapper", ".galary .move__mark");
-	carousel(".main__galary-wrapper", ".galary .move__mark");
+	carousel(".main__galary-wrapper", ".main__galary .move__mark");
 
 	Date.prototype.daysInMonth = function () {
 		return 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
